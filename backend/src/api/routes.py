55a6ef70bbbeeba3fd41c4611748261aa9459758
Mahimai Raja J <mahimairaja3@gmail.com>
2026-06-09 09:54:09 -0400

    Update routes.py

--- backend/src/api/routes.py
+++ backend/src/api/routes.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from src.api.endpoints.agent import router as agent_router
+from src.api.endpoints.patients import router as patients_router
 from src.api.endpoints.users import router as users_router
 
 routers = APIRouter(prefix="/v1")
 routers.include_router(users_router)
 routers.include_router(agent_router)
+routers.include_router(patients_router)
